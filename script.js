f7ef94f1b6cc1a21fc1381b181d0d76e095a7fb4
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 11:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form>
-        <input type='number' id='age' placeholder='Enter age'>
-        <input type='text' id='name' placeholder='Enter name'>
-        <button type='submit' id='btn'>Submit</button>
-    </form>
-    <script>
-        //your JS code here. If required.
-        let form = document.querySelector('form')
+    let form = document.querySelector('form')
         let ageBox = document.querySelector('#age')
         let nameBox = document.querySelector('#name')
         let btn = document.querySelector('#btn')
@@ -24,7 +7,7 @@
             let age = ageBox.value
             let name = nameBox.value
             if (!age || !name) {
-                alert('Please enter valid details.')
+                alert('Please enter valid details')
                 return
             }
             new Promise((resolve, reject)=>{
@@ -46,7 +29,3 @@
                 alert(err)
             })
         })
-    </script>
-</body>
-
-</html>
