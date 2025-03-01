9fb0cccfda7f146414f201475de02b52dc90a978
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 11:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+    let form = document.querySelector('form')
+        let ageBox = document.querySelector('#age')
+        let nameBox = document.querySelector('#name')
+        let btn = document.querySelector('#btn')
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let age = ageBox.value
+            let name = nameBox.value
+            if (!age || !name) {
+                alert('Please enter valid details.')
+                return
+            }
+            new Promise((resolve, reject)=>{
+                if(age>18){
+                    setTimeout(() => {
+                        resolve('Welcome, . You can vote.') 
+                    }, 4000)
+                }
+                else{
+                    setTimeout(() => {
+                        reject("Oh sorry . You aren't old enough.")
+                    }, 4000)
+                }
+            })
+            .then(data =>{
+                alert(data)
+            })
+            .catch(err =>{
+                alert(err)
+            })
+        })
