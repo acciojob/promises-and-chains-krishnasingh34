3f58666d46c7695024556a115405a067840cc128
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 11:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-    let form = document.querySelector('form')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <input type='number' id='age' placeholder='Enter age'>
+        <input type='text' id='name' placeholder='Enter name'>
+        <button type='submit' id='btn'>Submit</button>
+    </form>
+    <script>
+        //your JS code here. If required.
+        let form = document.querySelector('form')
         let ageBox = document.querySelector('#age')
         let nameBox = document.querySelector('#name')
         let btn = document.querySelector('#btn')
@@ -13,12 +30,12 @@
             new Promise((resolve, reject)=>{
                 if(age>18){
                     setTimeout(() => {
-                        resolve('Welcome, . You can vote.') 
+                        resolve('Welcome, '+name +'.'+ ' You can vote.') 
                     }, 4000)
                 }
                 else{
                     setTimeout(() => {
-                        reject("Oh sorry . You aren't old enough.")
+                        reject('Oh sorry '+name +'.'+ " You aren't old enough.")
                     }, 4000)
                 }
             })
@@ -29,3 +46,7 @@
                 alert(err)
             })
         })
+    </script>
+</body>
+
+</html>
